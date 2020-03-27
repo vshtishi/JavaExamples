252679cc2c4b409964f298ea04dda40bb491f13e
vshtishi <vjolashtishi@gmail.com>
2020-03-27 15:18:08 +0100

    Functional Programming (1)

--- src/main/java/com/github/vshtishi/Application.java
+++ src/main/java/com/github/vshtishi/Application.java
@@ -1,113 +1,86 @@
 package com.github.vshtishi;
 import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Application {
-	//Using unbounded wildcards
-	public static void printList(List<?> list){
-		for(Object x: list)
-			System.out.println(x);
-	}
-	
+	//Returning an Optional
+     public static Optional<Double> average(int... scores){
+    	 if(scores.length==0)
+    		 return Optional.empty();
+    	 int sum=0;
+    	 for(int score: scores)
+    		 sum += score;
+    	 return Optional.of((double)sum/scores.length);
+    	     
+     }
 	public static void main(String[] args){
-		Person person1=new Person("Ann",001);
-		Person person2=new Person("James",056);
-		Person person3=new Person("Sophie",007);
-		Employee employee1=new Employee("John",002);
-		List<Person> people=new ArrayList<>();
-		List<Employee> employees=new ArrayList<>();
-		people.add(person1);
-		people.add(person2);
-		people.add(person3);
-		employees.add(employee1);
-		printList(people);
-		printList(employees);
-		Collections.sort(people);
-		System.out.println("Sorted list using Comparable:");
-		printList(people);
+		//An Optional can be empty or contain a value
+		System.out.println(average(95,100));
+		System.out.println(average());
 		
-		//Sorting List by using Comparator
-		/*
-		 * Without using Lambdas:
-		    Comparator<Person> byName=new Comparator<Person>(){
-			public int compare(Person p1, Person p2){
-				return (p1.getName()).compareTo(p2.getName()); //Calling String's compareTo()
-			}
-		};
-		*/
-		Comparator<Person> byName=(p1,p2)-> (p1.getName()).compareTo(p2.getName());
-		Collections.sort(people, byName);
-		System.out.println("Sorted list using Comparator:");
-		printList(people);
+		//Checking if the Optional contains a value
+          Optional<Double> optional=average(50,100);
+          if(optional.isPresent()){
+        	  System.out.println(optional.get());
+          }
+	   
+        //If a value is null, we assign an empty Optional. Otherwise, we wrap the value
+          Double d=null;
+          Optional<Double> opt=Optional.ofNullable(d);
+          System.out.println("Empty optional assigned to a null value: " +opt);
 		
-		//Removing Conditionally
-		  people.removeIf(s-> s.getName().startsWith("A"));
-		  System.out.println("List after removing conditionally:");
-		  printList(people);
-		  
-		//Using upper-bounded wildcards
-		 List<? extends Person> list1=new ArrayList<Employee>();
-		 //Using lower-bounded wildcards
-		 List<? super Employee> list2=new ArrayList<Object>();
+        //Using Optional instance methods
+          optional.ifPresent(System.out::println); //Prints the value if it is present
+          System.out.println(opt.orElse(Double.NaN)); //Prints NaN if a value is not present
+          System.out.println(opt.orElseGet(()-> 0.0)); //Prints the result returned from Supplier
+          System.out.println(optional.orElseThrow(()-> new IllegalStateException()));  //Gets Supplier to create an Exception that should be thrown
+          
+       //Creating Stream sources
+          Stream<String> empty=Stream.empty(); //Creates an empty stream
+          Stream<Integer> singleElement=Stream.of(1); //Creates a stream with a single element
+          Stream<Integer> fromArray=Stream.of(1,2,3); //Creates a stream with multiple elements
+          //Converting from a list to a stream
+          List<Integer> list= Arrays.asList(1,2,3);
+          Stream<Integer> fromList= list.stream();
+          
+	   //Terminal stream operations	 
+		 System.out.println("The number of elements is: " +fromList.count());
 		 
-		 //Using the Set interface
-		 Student student1=new Student("Jane",101,Arrays.asList(5,6,7));
-		 Student student2=new Student("Harry",101,Arrays.asList(7,8,7));
-		 Student student3=new Student("Sarah",103,Arrays.asList(8,9,10));
-		 Set<Integer> set=new HashSet<>();
-		 set.add(student1.getStudentId());
-		 boolean b=set.add(student2.getStudentId());
-		 set.add(student3.getStudentId());
-		 System.out.println(b); //A set must preserve uniqueness
+		 Stream<String> s1=Stream.of("Ann", "Jane", "Harry");
+		 Optional<String> minimum= s1.min((s,t) -> s.length()-t.length());
+		 System.out.print("The minimum value of the stream is: ");
+		 minimum.ifPresent(System.out::println);
 		 
-		 System.out.println();
+		 Stream<String> s2=Stream.of("Ann", "Jane", "Harry");
+		 Optional<String> maximum= s2.max((s,t) -> s.length()-t.length());
+		 System.out.print("The maximum value of the stream is: ");
+		 maximum.ifPresent(System.out::println);
 		 
-		 //Using the Map interface
-		 Map<String,Integer> map=new HashMap<>();
-		 map.put(student1.getName(), student1.getStudentId());
-		 map.put(student2.getName(), student2.getStudentId());
-		 map.put(student3.getName(), student3.getStudentId());
-		 System.out.println("Student Map Keys: ");
-		 for(String k: map.keySet())
-			 System.out.print(k + " ");
-		 System.out.println(" ");
+	   	 Stream<Double> infinite=Stream.generate(Math::random);
+	   	 infinite.findAny().ifPresent(System.out::println);
+		 fromArray.findFirst().ifPresent(System.out::println);
 		 
-		 //Updating the value of a specific key in the map
-		 map.computeIfAbsent("Sarah",k->111);
-		 System.out.println("The value was not updated because the key was already present: " +map.get("Sarah"));
+		 List<Integer> list1=Arrays.asList(7,8,9,10);
+		 Predicate<Integer> pred= x -> x>10;
+		 System.out.print("Searching the stream :");
+		 System.out.print(list.stream().anyMatch(pred)+ " "); //There is no match for any element greater than 10 in the stream
+		 System.out.print(list.stream().noneMatch(pred)+ " "); //None of the elements match the condition
+		 pred= x -> x<=10;
+		 System.out.println(list.stream().allMatch(pred)); //All of the elements match the condition
+		 System.out.println("Printing the stream:");
+		 list1.stream().forEach(System.out::println);
 		 
-		 map.computeIfPresent("Sarah", (k,v) -> v+1);
-		 System.out.println("The value of the specified key was updated to: " +map.get("Sarah"));
+		 Stream<String> s3=Stream.of("John"," ","Doe");
+		 String fullName=s3.reduce("",String::concat);
+		 System.out.println("(1)Reducing the stream: " + fullName);
 		 
-		 /*
-		 Map<String,Integer> map2=new TreeMap<>();
-		 map2.put(student1.getName(), student1.getStudentId());
-		 map2.put(student2.getName(), student2.getStudentId());
-		 map2.put(student3.getName(), student3.getStudentId());
-		 System.out.println("Student Map Keys: ");
-		 for(String k: map2.keySet())
-			 System.out.print(k + ",");
-		*/
+		 Stream<Integer> s4=Stream.of(3,4,5);
+		 System.out.println("(2)Reducing the stream: " + s4.reduce(1, (a,b) -> a*b));
 		 
-		 //Using the Queue interface
-		 Queue<Integer> queue =new ArrayDeque<>();  //FIFO
-		 queue.offer(1);
-		 queue.offer(2);
-		 queue.offer(3);
-		 System.out.println("The first element: " + queue.peek());
-		 System.out.println("Removing first element: " + queue.poll());
-		 System.out.println("The first element now is: " + queue.peek());
-		 
-		 System.out.println();
-		 
-		 ArrayDeque<Integer> stack =new ArrayDeque<>(); //LIFO
-		 stack.push(1);
-		 stack.push(2);
-		 stack.push(3);
-		 System.out.println("The element on top of the stack: " + stack.peek());
-		 System.out.println("Removing the element on top: " + stack.poll());
-		 System.out.println("The first element now is: " + stack.peek());
-		 
-		 
-		 
+		 Stream<String> s5=Stream.of("s","t","r","e","a","m");
+		 TreeSet<String> set=s5.collect(Collectors.toCollection(TreeSet::new));
+		 System.out.println("Using Collectors: " + set);
     }
 }
